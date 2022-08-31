f5dc420697003d998fd22e8a20e5248e32a4826d
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-08-31 16:39:46 +0900

    feat/ 자유게시판 permission 추가

--- Wayne_Hills/Wayne_Hills/permissions.py
+++ Wayne_Hills/Wayne_Hills/permissions.py
@@ -8,28 +8,51 @@ class GenericAPIException(APIException):
         self.status_code=status_code
         super().__init__(detail=detail, code=code)
 
-class IsAdminOrIsAuthenticatedReadOnly(BasePermission):
+class IsAdminOrReadOnly(BasePermission):
     """
-    admin 사용자는 모두 가능, 로그인 사용자는 조회만 가능
+    admin 사용자는 모두 가능, 조회는 모두 가능
     """
     SAFE_METHODS = ('GET', )
-    message = '접근 권한이 없습니다.'
+
+    def has_permission(self, request, view):
+        user = request.user
+        if not self.SAFE_METHODS and not user.is_authenticated:
+            response ={"detail": "서비스를 이용하기 위해 로그인 해주세요."}
+            raise GenericAPIException(status_code=status.HTTP_401_UNAUTHORIZED, detail=response)
+        return bool(self.SAFE_METHODS or (user.is_authenticated and user.is_admin))
+
+class IsAuthenticatedOrReadOnly(BasePermission):
+    """
+    #자유게시판 get/post
+    조회: 모두 가능, 작성: 가입된 사람만
+    """
+    SAFE_METHODS = ('GET', )
 
     def has_permission(self, request, view):
         user = request.user
 
-        if not user.is_authenticated:
-            response ={
-                    "detail": "서비스를 이용하기 위해 로그인 해주세요.",
-                }
+        if not self.SAFE_METHODS and not user.is_authenticated:
+            response ={"detail": "서비스를 이용하기 위해 로그인 해주세요."}
             raise GenericAPIException(status_code=status.HTTP_401_UNAUTHORIZED, detail=response)
 
-        if user.is_authenticated and request.method in self.SAFE_METHODS: 
-            return True
-            
-        if user.is_authenticated and user.is_admin: 
-            return True
-        
-        return False
+        return bool(self.SAFE_METHODS or user.is_authenticated)
+
+
+class IsAdminOrIsAthenticatedPutOnly(BasePermission):
+    """
+    #자유게시판 put/delete
+    수정: 가입된 사람만, 삭제: 가입된 사람 및 운영자
+    """
+    SAFE_METHODS = ('PUT', )
+
+    def has_permission(self, request, view):
+        user = request.user
+        if not user.is_authenticated:
+            response ={"detail": "서비스를 이용하기 위해 로그인 해주세요."}
+            raise GenericAPIException(status_code=status.HTTP_401_UNAUTHORIZED, detail=response)
+
+        return bool(self.SAFE_METHODS or (user.is_authenticated and user.is_admin))
+
+
 
 
