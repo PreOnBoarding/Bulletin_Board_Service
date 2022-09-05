14ae995aee545d81c7bf1fdd1da5eae0479dcf76
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-09-05 14:46:02 +0900

    feat/ check_can_create_post 함수 검증 test 작성

--- posts/tests/services/test_permissions.py
+++ posts/tests/services/test_permissions.py
@@ -117,7 +117,8 @@ class TestPostPermission(TestCase):
 
     def test_check_can_get_post_with_correct_post_type_(self):
         """
-        게시판 Get에 대한 권한을 체크하는 check_can_get_post Service post type이 정확하게 출력되는 경우
+        게시판 Get에 대한 권한을 체크하는 check_can_get_post Service post type이 정확한지 검증
+        게시글 id 타입: int, float, True
         """
         true_post_type_id_list=[1, 3.5, True]
         user = UserModel.objects.get(username = "general")
@@ -125,9 +126,11 @@ class TestPostPermission(TestCase):
             self.assertEqual(check_can_get_post(item, user), True)
 
 
-    def test_check_can_get_post_with_incorrect_post_type_value_error(self):
+    def test_check_can_get_post_with_incorrect_post_type_printing_value_error(self):
         """
-        게시판 Get에 대한 권한을 체크하는 check_can_get_post Service post type이 정확하지 않아 value 에러를 출력할때
+        게시판 Get에 대한 권한을 체크하는 check_can_get_post Service 검증 
+        post type이 정확하지 않아 value 에러를 출력할때
+        게시글 id 타입: String
         """
         false_post_type_id_list=["","string"]
         user = UserModel.objects.get(username = "general")
@@ -135,9 +138,11 @@ class TestPostPermission(TestCase):
             with self.assertRaises(ValueError):
                 check_can_get_post(item, user)
 
-    def test_check_can_get_post_with_incorrect_post_type_type_error(self):
+    def test_check_can_get_post_with_incorrect_post_type_printing_type_error(self):
         """
-        게시판 Get에 대한 권한을 체크하는 check_can_get_post Service post type이 없거나 정확하지 않아 type error를 출력할때
+        게시판 Get에 대한 권한을 체크하는 check_can_get_post Service 검증 
+        post type이 없거나 정확하지 않아 type error를 출력할때
+        게시글 id 타입: 복소수
         """
         false_post_type_id_list=[2+5j]
         user = UserModel.objects.get(username = "general")
@@ -147,7 +152,9 @@ class TestPostPermission(TestCase):
 
     def test_check_can_get_post_with_incorrect_post_type_matching_query_error(self):
         """
-        게시판 Get에 대한 권한을 체크하는 check_can_get_post Service post type이 없거나 정확하지 않아 matching query error를 출력할때
+        게시판 Get에 대한 권한을 체크하는 check_can_get_post Service 검증
+        post type이 없거나 정확하지 않아 matching query error를 출력할때
+        게시글 id: post type에 해당하지 않는 정수
         """
         false_post_type_id_list=[-3,5]
         user = UserModel.objects.get(username = "general")
@@ -157,7 +164,9 @@ class TestPostPermission(TestCase):
 
     def test_check_can_get_post_with_incorrect_user_type_(self):
         """
-        게시판 Get에 대한 권한을 체크하는 check_can_get_post Service user type이 없거나 정확하지 않아 attribute error를 출력할때
+        게시판 Get에 대한 권한을 체크하는 check_can_get_post Service 검증
+        user type이 없거나 정확하지 않아 attribute error를 출력할때
+        게시글 id 타입: string, float, usertype에 해당하지 않는 정수
         """
         false_user_type_list=["string", 12, 3.5, ""]
         post = PostTypeModel.objects.get(post_type="General").id
@@ -228,8 +237,8 @@ class TestPostPermission(TestCase):
     def test_check_can_create_post_with_incorrect_user_type_(self):
         """
         게시판 POST 에 대한 권한을 체크하는 check_can_create_post Service 검증
-        게시글 타입 : Notice
-        유저 타입 : Manager
+        user type이 정확하지 않아 attribute error를 출력할때
+        게시글 타입 : string, float 복소수, post_type id에 해당하지 않는 정수
         """
         false_user_type_list=["string", 12, 3.5, ""]
         post = PostTypeModel.objects.get(post_type="General").id
@@ -239,13 +248,15 @@ class TestPostPermission(TestCase):
     def test_check_can_create_post_with_incorrect_post_type_(self):
         """
         게시판 POST 에 대한 권한을 체크하는 check_can_create_post Service 검증
-        게시글 타입 : Notice
-        유저 타입 : Manager
+        post type이 정확하지 않아 attribute error를 출력할때
+        게시글 타입 : string, float 복소수, post_type id에 해당하지 않는 정수
         """
-        false_post_type_list=["","string",3, 2+5j]
+        false_post_type_list=["","string",3, 2+5j, 1.5]
         user = UserModel.objects.get(username = "general")
         for item in false_post_type_list:
             with self.assertRaises(AttributeError):
                 check_can_get_post(user, item)
+
+
 
 
