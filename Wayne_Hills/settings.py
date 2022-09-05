8af6526d399b9d18462f8c5c364278e5f4e61f43
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-09-05 21:01:58 +0900

    refac/ 도메인 연결

--- Wayne_Hills/settings.py
+++ Wayne_Hills/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "Wayne")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG",1))
 
-ALLOWED_HOSTS = ['15.164.93.44', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['15.164.93.44', '127.0.0.1', 'localhost', 'citistagram.com', 'www.citistagram.com']
 
 
 # Application definition
